37dc72143ab9f99580543f96666b214e69d8a5cd
Jaakko Ketola <jaakko.ketola@aalto.fi>
2014-11-12 16:07:12 +0200

    remove redundant t, old_t

--- src/modinv.hh
+++ src/modinv.hh
@@ -3,17 +3,12 @@ int modinv(int a, int m) {
     int old_r = a;
     int s = 0;
     int old_s = 1;
-    int t = 1;
-    int old_t = 0;
 
     while (r != 0) {
         int q = old_r / r;
         int temp = s;
         s = old_s - q * s;
         old_s = temp;
-        temp = t;
-        t = old_t - q * t;
-        old_t = temp;
         temp = r;
         r = old_r - q * r;
         old_r = temp;
