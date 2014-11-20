62b33dbe97f3df66d928ac5962a073112ae8a2b3
jnalanko <alanko.jarno@gmail.com>
2014-11-20 17:33:55 +0200

    Add modular exponent

---
+++ src/mod_exp_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+#include "mod_exp.hh"
+
+int mod_exp_naive(int base, int exponent, int mod){
+    int result = 1;
+    for(int i = 0; i < exponent; i++){
+        result = (result * base) % mod;
+    }
+    return result;
+}
+
+int main(){
+    for(int i = 0; i <= 100; i++){
+        assert(mod_exp(2,i,101) == mod_exp_naive(2,i,101));
+        assert(mod_exp(i,i,101) == mod_exp_naive(i,i,101));
+    }
+    assert(mod_exp(3,32982391,101) == mod_exp_naive(3,32982391,101));
+    std::cout << "All tests OK" << std::endl;
+}
