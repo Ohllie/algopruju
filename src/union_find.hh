d75ad1493e1e5c1adf001227b8b6926ae70086e0
jnalanko <alanko.jarno@gmail.com>
2014-11-20 14:45:31 +0200

    Union find Formatting

--- src/union_find.hh
+++ src/union_find.hh
@@ -4,23 +4,22 @@ using namespace std;
 
 class UnionFind {
     public:
-        class UF_element {
-            public:
-                int size;
-                UF_element* parent;
-                int id;
-                UF_element(int id) : size(1), parent(this), id(id) {}
-        };
 
-        std::map<int, UF_element*> idToElement;
+    class UF_element {
+        public:
+            int size;
+            UF_element* parent;
+            int id;
+            UF_element(int id) : size(1), parent(this), id(id) {}
+    };
 
-    public:
-        void makeSet(int id); // Creates a new set with identifier id
-        int find(int id); // Returns the identifier of the set that contains the parameter id
-        void doUnion(int id_1, int id_2); // Merges the two sets given by the identifiers
-        int getSize(int id); // Returns the size of the set
+    std::map<int, UF_element*> idToElement;
+    void makeSet(int id); // Creates a new set with identifier id
+    int find(int id); // Returns the identifier of the set that contains the parameter id
+    void doUnion(int id_1, int id_2); // Merges the two sets given by the identifiers
+    int getSize(int id); // Returns the size of the set
 
-        ~UnionFind();
+    ~UnionFind();
 };
 
 
