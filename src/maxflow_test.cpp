140545dd9e1d539e3cafe542b77d80808cf5f613
Ketola Jaakko <jaakko.ketola@aalto.fi>
2014-11-20 14:08:51 +0200

    Max-flow/Min-cut

---
+++ src/maxflow_test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "maxflow.hh"
+using namespace std;
+
+int main()
+{
+    while (!cin.eof() && cin.good()) {
+        int n;
+        cin >> n;
+        Mat g(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> g(i,j);
+            }
+        }
+        
+        Mat result(n);
+        cout << maxflow(g, 0, n-1, result) << endl;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << result(i,j) << " ";
+            }
+            cout << endl;
+        }
+    }
+}
